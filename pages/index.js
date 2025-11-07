c6d3ecb3473ab3651b0ea81d1867eba54777a4e5
JahShoeAh <joshuacho52005@gmail.com>
2025-11-07 13:02:36 -0500

    still messing with phone layout

--- pages/index.js
+++ pages/index.js
@@ -114,8 +114,10 @@ export default function Home() {
       <div className="relative min-h-screen bg-terminal-base">
         <Navbar />
         <main className="relative">
-          <Hero />
-          <ResumeSection data={resumeData} />
+          <div className="mx-auto w-full max-w-6xl px-4 sm:px-6">
+            <Hero />
+            <ResumeSection data={resumeData} />
+          </div>
         </main>
       </div>
     </>
